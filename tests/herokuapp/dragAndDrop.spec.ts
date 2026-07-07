dd20faac90fbe78e3131ac6e8194cef65c882d38
Ramesh Ch <rameshn3@gmail.com>
2026-07-07 08:32:23 +0530

    herokuapp changs

--- tests/herokuapp/dragAndDrop.spec.ts
+++ tests/herokuapp/dragAndDrop.spec.ts
@@ -7,8 +7,8 @@ test.describe('drag and drop scenario', () => {
         await herokuAppPage.navigateToDragAndDrop(); // Navigate to the Draganddrop page
     });
 
-    test('should download a file', async ({ herokuAppPage }) => {
-     const download = await herokuAppPage.dragAndDrop();
+    test('should drag source to target', async ({ herokuAppPage }) => {
+         await herokuAppPage.dragAndDrop();
 
         const sourceText = await herokuAppPage.getColumnAText();
         const targetText = await herokuAppPage.getColumnBText();
